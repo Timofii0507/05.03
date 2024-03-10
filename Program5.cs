adb39a863d1b3131d065fe10ebc1ea568e1d9b62
Timofii0507 <timofiiyurkov@gamil.com>
2024-03-10 23:49:35 +0200

    Singleton

---
+++ Program5.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.CursorVisible = false;
+            Singleton singleton = Singleton.GetInstance();
+            singleton.DoAction();
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05._03
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        protected Singleton() { }
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+
+        public void DoAction()
+        {
+            Console.WriteLine("Singleton does something.");
+        }
+    }
+}
